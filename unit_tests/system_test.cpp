004b4ee9a0c46ae5eaa3f1f85cee5c4661b6c7bb
Osama Muhammad <osamamohammed3623@gmail.com>
2023-01-24 09:43:26 +0200

    write the test case for load/store functions

--- unit_tests/system_test.cpp
+++ unit_tests/system_test.cpp
@@ -3,36 +3,11 @@
 #include <fstream>
 using namespace std;
 
-TEST(Password_Control, test_load_password){
-    // use System function to read, write with a stream
+TEST(Password_Control, test_store_load_password){
+    // test the load/store functions at once
+    string p = "this is a test password";
+    System::store_password(p, "../user_pass");
 
-    string test_pass = "testing System::load_password";
-    
-    fstream file;
-    file.open("../user_pass");
-    
-    if(file.is_open()){
-        file << test_pass;
-    }
-
-    string stored_pass = System::load_password();
-
-    ASSERT_STREQ(test_pass.c_str(), stored_pass.c_str());
+    string stored_p = System::load_password("../user_pass");
+    ASSERT_STREQ(p.c_str(), stored_p.c_str());
 }
-
-// TEST(Password_Control, test_store_password){
-//     // use System function to write, read with a stream
-
-//     string test_pass = "testing System::store_password";
-//     System::store_password(test_pass);
-
-//     fstream file;
-//     file.open("../user_pass");
-    
-//     string stored_pass{};
-//     if(file.is_open()){
-//         file >> stored_pass;
-//     }
-
-//     ASSERT_STREQ(test_pass.c_str(), stored_pass.c_str());
-// }
