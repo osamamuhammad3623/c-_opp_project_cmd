a39c0ef422b4ad649b221a8b93f3141d91684754
Osama Muhammad <osamamohammed3623@gmail.com>
2023-01-24 20:42:49 +0200

    remove change_password function and define display_search_result template in the header file

--- system.h
+++ system.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 namespace System{
@@ -7,8 +9,6 @@ namespace System{
 
     void wait_for_user(void);
 
-    void change_password(void);
-
     bool user_login(void);
 
     string load_password(string file_name="user_pass");
@@ -16,4 +16,18 @@ namespace System{
     void store_password(string pass, string file_name="user_pass");
 
     int take_user_choice(void);
+
+    /* definition is in the header file, visit: https://stackoverflow.com/questions/14005060/undefined-reference-to */
+    template<typename T>
+    void display_search_result(vector<T> &result){
+        if (!result.empty()){
+            system("clear");
+            cout << "Search results:\n";
+            for(T &obj: result){
+                obj.display();
+            }
+        }else{
+            cout << "No results\n";
+        }
+    }
 }
